e453df9ad5cb151d4341c046fc51fc19965ec912
Gatoze <57226465+Gatoze@users.noreply.github.com>
2022-01-16 20:21:19 +0100

    actividad_navideña

--- app/src/main/java/com/fcm/paises/MainActivity.java
+++ app/src/main/java/com/fcm/paises/MainActivity.java
@@ -4,18 +4,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.fcm.paises.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    AcercaDeFragment info = new AcercaDeFragment();
+    SettingsFragment config = new SettingsFragment();
+    PaisFragment main = new PaisFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +31,43 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
         setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        loadFragment(main);
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.AcercaDeFragment:
+                    loadFragment(info);
+                    break;
+                case R.id.settingsFragment:
+                    loadFragment(config);
+                    break;
+                case R.id.paisFragment:
+                    loadFragment(main);
+                    break;
+            }
+            return true;
+        });
+        /*
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        }
+    });
+         */
+}
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.navegador_principal, fragment);
+        transaction.commit();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,4 +87,5 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
